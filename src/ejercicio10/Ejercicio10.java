ebfb4d959fff58ed5a987f35e32f4eb0a683d005
Ingenieria <Ingenieria@DESKTOP-17V176H>
2019-12-20 11:34:55 -0600

    Ejercicio #10

---
+++ src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Ejercicio10 {
+
+    /**
+     * Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+     */
+    public static void main(String[] args) {
+       
+        int Num=1;
+        
+        while(Num<=100)
+             {
+                 System.out.print(Num);
+                 System.out.print(" ");
+                 Num++;
+             }
+    }
+    
+}
